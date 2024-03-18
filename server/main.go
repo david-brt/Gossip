0cb390af1c71dc10cd77ade5a2d36831ce73ea64
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-03-18 21:25:23 +0100

    add signup and login route

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Gossip/backend/handlers"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -11,6 +12,12 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/signup", func(c *gin.Context) {
+		handlers.Signup(c)
+	})
+	r.GET("/login", func(c *gin.Context) {
+		handlers.Login(c)
 	})
 	r.Run(":8910")
 }
