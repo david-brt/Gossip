1c3f6bfc2df27bf34aacdd5e3e0bf664ebff4994
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-04-06 22:28:01 +0200

    add signup page spacing

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,16 +1,19 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthContextProvider from "../context/AuthContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
-const RootLayout = () => {
+const Layout = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <Stack />
-      </AuthContextProvider>
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </AuthContextProvider>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 };
 
-export default RootLayout;
+export default Layout;
