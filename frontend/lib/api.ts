c9af10b7e3e3584e134b40f5eb2203ae0f147328
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-04-12 22:02:06 +0200

    store credentials on device

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,9 +1,28 @@
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+async function save(key: string, value: string) {
+  try {
+    await SecureStore.setItemAsync(key, value);
+  } catch (e) {
+    throw e;
+  }
+}
+async function store(key: string, value: string) {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    throw e;
+  }
+}
 
 export async function createUser(phoneNumber: string) {
   const route = `${process.env.EXPO_PUBLIC_DATA_ROUTE}/signup`;
   try {
-    await axios.post(route, { number: phoneNumber });
+    const res = await axios.post(route, { number: phoneNumber });
+    save("token", res.data.session);
+    store("phone-number", phoneNumber);
   } catch (e) {
     throw e;
   }
