3cf5f6e88ea588606b21437796ba6b345e6397d9
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-05-27 00:12:26 +0200

    add index to contact list

--- frontend/app/(auth)/(tabs)/chats/new-chat.tsx
+++ frontend/app/(auth)/(tabs)/chats/new-chat.tsx
@@ -14,6 +14,62 @@ async function getContacts() {
   }
 }
 
+function normalize(name: string | undefined) {
+  if (name === undefined) {
+    return "";
+  }
+  return name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function sortContacts(contacts: Contacts.Contact[]) {
+  return contacts.sort((a, b) => {
+    const lastNameA = normalize(a.lastName);
+    const lastNameB = normalize(b.lastName);
+    const firstNameA = normalize(a.firstName);
+    const firstNameB = normalize(b.firstName);
+
+    if (lastNameA < lastNameB) return -1;
+    if (lastNameA > lastNameB) return 1;
+
+    if (firstNameA < firstNameB) return -1;
+    if (firstNameA > firstNameB) return 1;
+
+    return 0;
+  });
+}
+
+function getStartingLetter(contact: Contacts.Contact) {
+  let relevantName = contact.firstName;
+  if (contact.firstName === undefined) {
+    relevantName = contact.lastName;
+  }
+  if (contact.contactType === "company") {
+    relevantName = contact.company;
+  }
+  relevantName = relevantName as string;
+
+  let startingLetter = normalize(relevantName[0].toUpperCase());
+  if (!/[A-Z]/.test(startingLetter)) {
+    startingLetter = "#";
+  }
+  return startingLetter;
+}
+
+function sortAndIndex(contacts: Contacts.Contact[]) {
+  sortContacts(contacts);
+  let currentIndex = "";
+  let res: Array<Contacts.Contact | string> = [];
+  contacts.forEach((contact) => {
+    const startingLetter = getStartingLetter(contact);
+    if (currentIndex !== startingLetter) {
+      currentIndex = startingLetter;
+      res.push(startingLetter);
+    }
+    res.push(contact);
+  });
+  return res;
+}
+
 const NewChat = () => {
   const { data } = useQuery({ queryKey: ["contacts"], queryFn: getContacts });
 
@@ -21,8 +77,13 @@ const NewChat = () => {
     <View style={styles.container}>
       {data && (
         <FlashList
-          data={data}
-          renderItem={({ item }) => <ContactCell item={item} />}
+          data={sortAndIndex(data)}
+          renderItem={({ item }) => {
+            if (typeof item === "string") {
+              return <Text>{item}</Text>;
+            }
+            return <ContactCell item={item} />;
+          }}
           estimatedItemSize={50}
         />
       )}
