0fba67c939b0e9064beeba67bf5bbcad9481674d
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-06-19 23:05:43 +0200

    add websocket server

--- server/go.mod
+++ server/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- server/main.go
+++ server/main.go
@@ -4,22 +4,24 @@ import (
 	"Gossip/backend/handlers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8081"},
-		AllowMethods:     []string{"POST"},
-		AllowHeaders:     []string{"Origin", "content-type"},
-		ExposeHeaders:    []string{"Content-Length"},
+		AllowAllOrigins: true,
+		// AllowOrigins:    []string{"http://localhost:8082"},
+		AllowMethods:     []string{"POST", "GET"},
+		AllowHeaders:     []string{"Origin", "content-type", "Accept-Encoding", "Accept-Language", "Cache-Control", "Connection", "Host", "Origin", "Pragma", "Sec-Websocket-Extensions", "Sec-Websocket-Key", "Sec-Websocket-Version", "Upgrade", "User-Agent"},
+		AllowWebSockets:  true,
 		AllowCredentials: true,
 	}))
+	hub := newHub()
+	go hub.run()
 	r.GET("/ws", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		w := c.Writer
+		r := c.Request
+		serveWs(hub, w, r)
 	})
 	r.POST("/signup", func(c *gin.Context) {
 		handlers.Signup(c)
